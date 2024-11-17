8fc58576b5c4705557dcbbe79950a0538a780873
XuanzhaoS <t3zhxu00@students.oamk.fi>
2024-11-18 01:21:44 +0200

    Style MovieCards

--- src/components/Home_Components/MovieCards.js
+++ src/components/Home_Components/MovieCards.js
@@ -4,20 +4,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function MovieCards({ movieList }) {
   return (
     <div className="container">
-      {movieList.map((movie) => (
-        <div className="card h-100" key={movie.id}>
-          {/* Move Poster */}
-          <img src={movie.image} className="card-img-top" alt={movie.title} />
-          <div className="card-body">
-            {/* Movie Title */}
-            <h3 className="card-title">{movie.title}</h3>
-            {/* Movie Rating */}
-            <p className="card-text">
-              <strong>Rating:</strong> {movie.rating}
-            </p>
+      <div className="row">
+        {movieList.map((movie) => (
+          <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={movie.id}>
+            <div className="card h-100" key={movie.id}>
+              {/* Move Poster */}
+              <img
+                src={movie.image}
+                className="card-img-top"
+                alt={movie.title}
+              />
+              <div className="card-body">
+                {/* Movie Title */}
+                <h3 className="card-title">{movie.title}</h3>
+                {/* Movie Rating */}
+                <p className="card-text">
+                  <strong>Rating:</strong> {movie.rating}
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
