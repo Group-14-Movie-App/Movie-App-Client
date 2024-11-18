6ed4ea973ed80373c25ef32ecdafe63a82a71172
t3jehe00 <t3jehe00@students.oamk.fi>
2024-11-18 04:13:00 +0200

    frontend of Register and SignIn page

--- src/screens/SignInPage.js
+++ src/screens/SignInPage.js
@@ -1,24 +1,41 @@
 import { useNavigate } from 'react-router-dom';
+import './screensStyles/SignInPage.css';
 
-function SignInPage()
-{
-    const navigate = useNavigate();
+function SignInPage() {
+  const navigate = useNavigate();
 
-    const handleRegisterRedirect = () => {
-        navigate('/register-page'); // Redirect to Register Page
-      };
+  const handleRegisterRedirect = () => {
+    navigate('/register-page'); // Redirect to Register Page
+  };
 
-    return(
-        <div>
-            <h1>This is Sign In Page</h1>
+  const handleLogin = () => {
+    // Handle login action here
+  };
 
-            {/* Redirect to Register Page */}
-            <div className="register-redirect">
-                <p>Don't have an account?</p>
-                <button onClick={handleRegisterRedirect} className="register-button">Register Here</button>
-            </div>
+  return (
+    <div className="signin-container">
+      <h1 className="signin-title">Sign In</h1>
+      
+      <form className="signin-form">
+        <div className="form-group">
+          <label htmlFor="email">Enter Email</label>
+          <input type="email" id="email" name="email" required />
         </div>
-    )
+        
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+
+        <button type="button" className="login-button" onClick={handleLogin}>Login</button>
+      </form>
+
+      <div className="register-redirect">
+        <p>Don't have an account?</p>
+        <button onClick={handleRegisterRedirect} className="register-button">Register</button>
+      </div>
+    </div>
+  );
 }
 
 export default SignInPage;
